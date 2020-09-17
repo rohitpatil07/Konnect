1be8b339c4f7a2219232e3cf9f9af83e89a21ea1
Rohit <rohitpatil07122001@gmail.com>
2020-09-17 16:24:28 +0530

    Add basic graph functions
    Signed-off-by: Rohit <rohitpatil07122001@gmail.com>

--- player/graph/Graph.java
+++ player/graph/Graph.java
@@ -7,14 +7,35 @@ import java.util.HashMap;
 
 class Graph{
 
-    private ArrayList<Vertex> VertexList; 
-    private ArrayList<Edge> EdgeList; 
-    private HashMap<Vertex,Edge> map; 
+    private ArrayList<Vertex> VertexList;
+    private ArrayList<Edge> EdgeList;
 
     public Graph(){
         this.VertexList = new ArrayList<Vertex>();
         this.EdgeList = new ArrayList<Edge>();
-        this.map = new HashMap<Vertex,Edge>();
+    }
+
+    public int numVertices(){
+       return VertexList.length();
+    }
+
+    public ArrayList<Vertex> vertices(){
+      ArrayList<Vertex> vertexList = new ArrayList<Vertex>();
+      for(Vertex v : VertexList){
+        vertexList.add(v);
+      }
+      return vertexList;
+    }
+
+    public int numEdges(){
+       return EdgeList.length();
+    }
+
+    public ArrayList<Edge> edges(){
+      ArrayList<Edge> edgeList = new ArrayList<Edge>();
+      for(Edge e : edgeList){
+        edgeList.add(e);
+      }
+       return edgeList;
     }
 }
-
